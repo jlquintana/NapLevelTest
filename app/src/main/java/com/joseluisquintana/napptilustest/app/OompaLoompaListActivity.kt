d5b6d355653ae985aafd9c32a950ed8745d7365d
jlquintana <joseluisquintanacabrera@gmail.com>
2017-11-15 11:25:57 +0100

    Create project structure

---
+++ app/src/main/java/com/joseluisquintana/napptilustest/app/OompaLoompaListActivity.kt
@@ -0,0 +1,12 @@
+package com.joseluisquintana.napptilustest.app
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+class OompaLoompaListActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_oompa_loompa_list)
+    }
+}
